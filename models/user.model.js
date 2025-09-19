258caa27352fdffc65355e0775f757637bd2c54d
rajeev <rajeevdahiya435@gmail.com>
2025-09-20 03:14:40 +0530

    First commit

---
+++ models/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const userSchema = new mongoose.Schema({
+
+    fullname: {
+
+        firstname: {type: String,
+                    required: true, 
+                    minlength:[3, 'First name must be at least 3 characters long']
+                },
+        lastname: {type: String, required: true}
+    },
+
+    email: {type: String, 
+            required: true, 
+            unique: true, 
+            minlength:[5, 'Email must be at least 5 characters long']
+        },
+    password: {type: String, required: true, select: false},
+
+    socketID: {type: String, default: null},
+})
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+    return token;
+}
+
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.static.hashPassword = async function(password){
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.modules.User || mongoose.model('User', userSchema) ;
+
+module.exports = userModel;
